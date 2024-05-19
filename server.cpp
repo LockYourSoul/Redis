7210072d18258c459655757e55490e7fd69c20df
Ritwij kumar <ritwijkumar@Ritwijs-MacBook-Pro.local>
2024-05-20 00:01:48 +0530

    updated code for protocol parsing

--- server.cpp
+++ server.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,74 +8,131 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <iostream>
 
-using namespace std;
 
 static void msg(const char *msg) {
-    cerr << msg << endl;
     fprintf(stderr, "%s\n", msg);
-    //use sterr for C
 }
 
-static void die(const char * msg){
-    //error number for the last system call
+static void die(const char *msg) {
     int err = errno;
-    cerr << "[" << err << "] " << msg << endl;
+    fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
 }
 
-static void do_something(int connfd){
-    char rbuf[64] = {};
-    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-    if(n<0){
-        msg("read() error");
-        return;
-    }
-    else{
-        cout << "client says: " << rbuf << endl;
-    }
+const size_t k_max_msg = 4096;
 
-    char wbuf[] = "World!";
-    write(connfd, wbuf, strlen(wbuf));
+static int32_t read_full(int fd, char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
-int32_t main(){
-    //creating a socket using IPv4 and TCP and 0 is for auto selection of protocol
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-    // configuiring different aspects of the socket
-    //RESUSEADDR server will be able to bind to the address if it's restarted
+static int32_t one_request(int connfd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
+            msg("EOF");
+        } else {
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");
+        return -1;
+    }
+
+    // request body
+    err = read_full(connfd, &rbuf[4], len);
+    if (err) {
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    rbuf[4 + len] = '\0';
+    printf("client says: %s\n", &rbuf[4]);
+
+    // reply using the same protocol
+    const char reply[] = "world";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
+
+int main() {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    // this is needed for most server applications
     int val = 1;
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
-    //let's bind the socket to some random address for now
+    // bind
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = ntohs(1234); // random port 1234 //network to host
-    addr.sin_addr.s_addr = ntohl(0); // random IP 0.0.0.0
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);    // wildcard address 0.0.0.0
     int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
-    if(rv){
+    if (rv) {
         die("bind()");
     }
 
-    //listen
-    rv = listen(fd, SOMAXCONN); //SOMAXCONN to allow maximum number of client to join at a time
-    if(rv){
+    // listen
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
         die("listen()");
     }
-    //loop to do something
 
-    while(true){
-        //accept
+    while (true) {
+        // accept
         struct sockaddr_in client_addr = {};
         socklen_t socklen = sizeof(client_addr);
         int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
-        if(connfd<0){
-            continue; //error
+        if (connfd < 0) {
+            continue;   // error
         }
 
-        do_something(connfd);
+        while (true) {
+            // here the server only serves one client connection at once
+            int32_t err = one_request(connfd);
+            if (err) {
+                break;
+            }
+        }
         close(connfd);
     }
+
+    return 0;
 }
